e9e9aa2ee404ac4cf063448f4fb38dade4eb1a19
ArielColqueH <arielcolque444@gmail.com>
2022-07-03 16:20:55 -0400

    se realizo la pantalla de signup

--- src/app/modules/home/pages/login-module/welcome-page/signup-component/signup-component.component.ts
+++ src/app/modules/home/pages/login-module/welcome-page/signup-component/signup-component.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
 @Component({
   selector: 'app-signup-component',
   templateUrl: './signup-component.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SignupComponentComponent implements OnInit {
+  iconSrc = 'assets/images/icon-google.svg';
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  value = 'Clear me';
 
-  constructor() { }
+  matcher = new MyErrorStateMatcher();
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
