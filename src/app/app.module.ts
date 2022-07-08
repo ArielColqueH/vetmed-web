ed7474c51b1996b2bf10db025f23990d15bfd8ec
ArielColqueH <arielcolque444@gmail.com>
2022-07-08 13:36:01 -0400

    se agrego el subsidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from './modules/home/home.module';
+import { SubsidenavComponent } from './layout/subsidenav/subsidenav.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule],
 
   providers: [],
   bootstrap: [AppComponent],
