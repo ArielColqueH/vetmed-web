d293945ac5f050bf8ca81095d83387d1a5c16b65
ArielColqueH <arielcolque444@gmail.com>
2022-07-10 10:52:43 -0400

    se agrego primera pantalla

--- src/app/modules/home/pages/login-module/signup-page/first-step-component/first-step-component.component.ts
+++ src/app/modules/home/pages/login-module/signup-page/first-step-component/first-step-component.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-first-step-component',
   templateUrl: './first-step-component.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class FirstStepComponentComponent implements OnInit {
-
-  constructor() { }
+  title = 'google-maps';
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    let loader = new Loader({
+      apiKey: 'AIzaSyBhPF9FSrEC3bncF7cSQ2RLEpCJnnY2yLg',
+    });
 
+    loader.load().then(() => {
+      new google.maps.Map(document.getElementById('map')!, {
+        center: { lat: 51.233334, lng: 6.78333 },
+        zoom: 6,
+      });
+    });
+  }
 }
